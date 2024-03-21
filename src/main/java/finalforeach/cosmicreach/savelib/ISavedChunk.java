ba8ef6653bf7cb383bb5460fa3a821b20edb1f95
FinalForEach <finalforeach@gmail.com>
2024-03-21 21:31:54 +0000

    Add methods to ISavedChunk

--- src/main/java/finalforeach/cosmicreach/savelib/ISavedChunk.java
+++ src/main/java/finalforeach/cosmicreach/savelib/ISavedChunk.java
@@ -1,7 +1,16 @@
 package finalforeach.cosmicreach.savelib;
 
+import finalforeach.cosmicreach.savelib.blockdata.IBlockData;
+
 public interface ISavedChunk {
 	public static final int CHUNK_WIDTH = 16;
 	public static final int NUM_BLOCKS_IN_CHUNK = CHUNK_WIDTH * CHUNK_WIDTH * CHUNK_WIDTH;
+	public boolean isEntirelyOpaque();
+	public boolean isEntirelyOneBlockSelfCulling();
+	public int getMaxNonEmptyBlockIdxYXZ();
+	public int getBlockX();
+	public int getBlockY();
+	public int getBlockZ();
+	public IBlockData<?> getBlockData();
 
 }
