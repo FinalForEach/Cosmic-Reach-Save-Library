9e9a03335bf3b2999aa3fb3a7cc246ae85bcef06
FinalForEach <finalforeach@gmail.com>
2024-09-03 04:21:06 +0100

    Revert to nibble default until bit layers are fixed properly

--- src/main/java/finalforeach/cosmicreach/savelib/blockdata/layers/BlockSingleLayer.java
+++ src/main/java/finalforeach/cosmicreach/savelib/blockdata/layers/BlockSingleLayer.java
@@ -42,7 +42,8 @@ public class BlockSingleLayer<T> implements IBlockLayer<T>
 			return;
 		}else 
 		{
-			final var layer = new BlockBitLayer<T>(chunkData, localY, this.blockValue);
+			//final var layer = new BlockBitLayer<T>(chunkData, localY, this.blockValue);
+			final var layer = new BlockNibbleLayer<T>(chunkData, localY, this.blockValue);
 			chunkData.setLayer(localY, layer);
 			layer.setBlockValue(chunkData, blockState, localX, localY, localZ);
 		}
