b493cf5f2f3ccf41e3b2c4044ac3e58537e78cf0
FinalForEach <finalforeach@gmail.com>
2025-04-23 02:03:25 +0100

    Add number type

--- src/main/java/finalforeach/cosmicreach/savelib/SaveFileConstants.java
+++ src/main/java/finalforeach/cosmicreach/savelib/SaveFileConstants.java
@@ -1,7 +1,6 @@
 package finalforeach.cosmicreach.savelib;
 
-
-public class SaveFileConstants 
+public class SaveFileConstants
 {
 	// [F]inal[F]or[E]ach [C]osmi[c]r[eac]h
 	public static final int MAGIC = 0xFFECCEAC;
@@ -9,8 +8,8 @@ public class SaveFileConstants
 	public static final int COMPRESSION_TYPE_NONE = 0;
 	public static final int COMPRESSION_TYPE_LZ4 = 1;
 
-	public static int PREFERRED_COMPRESSION_TYPE = COMPRESSION_TYPE_NONE;//COMPRESSION_TYPE_LZ4;
-	
+	public static int PREFERRED_COMPRESSION_TYPE = COMPRESSION_TYPE_NONE;// COMPRESSION_TYPE_LZ4;
+
 	public static final int BLOCK_NULL = 0;
 	public static final int BLOCK_SINGLE = 1;
 	public static final int BLOCK_LAYERED = 2;
@@ -22,28 +21,64 @@ public class SaveFileConstants
 	public static final int BLOCK_LAYER_BYTE = 5;
 	public static final int BLOCK_LAYER_SHORT = 6;
 	public static final int BLOCK_LAYER_BIT = 7;
-	
+
 	public static final int SKYLIGHTDATA_NULL = 1;
 	public static final int SKYLIGHTDATA_LAYERED = 2;
 	public static final int SKYLIGHTDATA_SINGLE = 3;
 
 	public static final int SKYLIGHTDATA_LAYER_SINGLE = 1;
 	public static final int SKYLIGHTDATA_LAYER_NIBBLE = 2;
-	
+
 	public static final int BLOCKLIGHTDATA_NULL = 1;
 	public static final int BLOCKLIGHTDATA_LAYERED = 2;
 
 	public static final int BLOCKLIGHTDATA_LAYER_SINGLE = 1;
 	public static final int BLOCKLIGHTDATA_LAYER_SHORT = 2;
-	
+
 	public static final int BLOCKLIGHTDATA_LAYER_MONO_NIBBLE_RED = 3;
 	public static final int BLOCKLIGHTDATA_LAYER_MONO_NIBBLE_GREEN = 4;
 	public static final int BLOCKLIGHTDATA_LAYER_MONO_NIBBLE_BLUE = 5;
-	
+
 	public static final int BLOCKENTITY_NULL = 0;
 	public static final int BLOCKENTITY_DATA = 1;
-	
+
 	public static final int ENTITIES_NULL = 0;
 	public static final int ENTITIES_DATA = 1;
-	
+
+	public enum NumberType
+	{
+		BYTE(1, 1), SHORT(2, 2), INT(3, 4), LONG(4, 8);
+
+		public byte id;
+		public int numBytes;
+
+		NumberType(int id, int numBytes)
+		{
+			this.id = (byte) id;
+			this.numBytes = numBytes;
+		}
+
+		public static NumberType get(byte id)
+		{
+			switch (id)
+			{
+			case 1:
+				return BYTE;
+			case 2:
+				return SHORT;
+			case 3:
+				return INT;
+			case 4:
+				return LONG;
+			default:
+				throw new RuntimeException("Unknown number type for id: " + id);
+			}
+		}
+
+		public int numBytes()
+		{
+			return numBytes;
+		}
+	}
+
 }
